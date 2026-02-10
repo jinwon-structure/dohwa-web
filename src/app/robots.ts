d531cbbcf000e2b65d501d7d63029fcc1e949185
Dohwa Engineering <dohwa@kakao.com>
2026-02-10 09:26:32 +0900

    fix: 도메인을 www.dohwa.pro로 통일 (robots.txt 미발견 수정)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/private/',
         },
-        sitemap: 'https://dohwa.pro/sitemap.xml',
+        sitemap: 'https://www.dohwa.pro/sitemap.xml',
     };
 }
